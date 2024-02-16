0175dc18ace9227370876235e0beaa31c6b97fc5
Michael <mmcshinsky@gmail.com>
2024-02-16 11:43:57 -0700

    move balance call to page server for nextjs build

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
 import { FaucetClient } from "@/components";
+import rpcClient from "@/utils/rpc";
 
 export default async function Home() {
-  const faucet = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/balance`).then(
-    (x) => x.json()
+  const balance = await rpcClient.request("getbalance", {});
+  const address = await rpcClient.request("getaccountaddress", {
+    account: "",
+  });
+  const transactions = await rpcClient.request("listtransactions", {
+    count: 10,
+  });
+
+  return (
+    <FaucetClient
+      faucet={{
+        balance,
+        address,
+        transactions,
+      }}
+    />
   );
-  return <FaucetClient faucet={faucet} />;
 }
